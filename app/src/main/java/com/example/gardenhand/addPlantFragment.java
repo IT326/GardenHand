d6bb1894b3681514511b66220e3f4715fc85a223
Tjpraka <Tjpraka@ilstu.edu>
2020-11-29 13:28:44 -0600

    made adding plants interface a tinge nicer

--- app/src/main/java/com/example/gardenhand/addPlantFragment.java
+++ app/src/main/java/com/example/gardenhand/addPlantFragment.java
@@ -42,47 +42,21 @@ public class addPlantFragment extends Fragment {
         final ListView listv = view.findViewById(R.id.plantList);
 
         final Button buttv =  view.findViewById(R.id.button_addplant);
+        ArrayList<Plant>  plantList = new ArrayList<Plant>();
 
-        view.findViewById(R.id.button_addplant).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-               // PlantApiCaller apicall = new PlantApiCaller();
-                Search searcher = new Search("Carrot",addPlantFragment.this.getContext());
-                searcher.findPlant(new VolleyCallback() {
-                    @Override
-                    public void onSuccess(JSONArray result) {
-                        try {
-                            //result is json Array
-
-                            JSONObject tempjso = null;
-                            ArrayList<Plant>  plantList = new ArrayList<Plant>();
-
-                            //for each in result add to list view
-                            for (int i =0; i< result.length() && i< 10; i++){
-                                tempjso = result.getJSONObject(i);
-                                plantList.add(new Plant(tempjso.getString("common_name"),tempjso.getString("image_url"),tempjso.getInt("id")));
-                                System.out.print(plantList.get(i).toString());
-
-                            }
-
-                           // String plantName = result.getString("common_name");
-                            text.setText("list complete"+ plantList.size());
-                            PlantListAdapter plAdapter = new PlantListAdapter(addPlantFragment.this.getContext(),plantList);
-                            //CustomAdapter customAdapter = new CustomAdapter(this, arrayList);
-                            listv.setAdapter(plAdapter);
-                            //text.setVisibility(View.INVISIBLE);
-                           // buttv.setVisibility(View.INVISIBLE);
+        plantList.add(new Plant("Garden tomato","https://bs.floristic.org/image/o/400851a79391dbe6f667c66e4bf70299e9921853",182512));
+        plantList.add(new Plant("Cayenne pepper","https://bs.floristic.org/image/o/19b8d627527f56c94e2eb1cb34ed2edcb4e2b5c9",115653));
+        plantList.add(new Plant("Garden lettuce","https://bs.floristic.org/image/o/7ee395463413655228bdb83cc086dd662d2b633c",147167));
+        plantList.add(new Plant("Garden pea","https://bs.floristic.org/image/o/de42bcb86806a72660b5d46d06b2ae40e25610d2",167361));
+        plantList.add(new Plant("Garden strawberry","https://bs.floristic.org/image/o/ade4fd72bdaebacc076be2072f515ef8596558fa",1056117));
+        plantList.add(new Plant("Garden thyme","https://bs.floristic.org/image/o/8709185087a881e96ffc55ce766b53f7e861cb3b",187946));
+        plantList.add(new Plant("Garden asparagus","https://bs.floristic.org/image/o/673e603f2370b1eddb2e5ec28b0818b73e5a594e",108462));
+        plantList.add(new Plant("Garden cucumber","https://bs.floristic.org/image/o/d278dfc3db5de3552484da316ab5925b75f1bea5",125316));
+        plantList.add(new Plant("Garden rhubarb","https://bs.floristic.org/image/o/caff92d8413a0d92c7111165d8f14118395997a0",174243));
+        PlantListAdapter plAdapter = new PlantListAdapter(addPlantFragment.this.getContext(),plantList);
+        listv.setAdapter(plAdapter);
 
 
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                });
-
-
-            }
-        });
     }
 
 
