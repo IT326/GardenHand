9916cbf71034cb286fd619453499d0dabc6b6d08
Wenkus, Nicholas <nwenkus@ilstu.edu>
2020-12-03 18:25:18 -0600

    New Activities

---
+++ app/src/main/java/com/example/gardenhand/First5Fragment.java
@@ -0,0 +1,35 @@
+package com.example.gardenhand;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+
+import androidx.navigation.fragment.NavHostFragment;
+
+public class First5Fragment extends Fragment {
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first5, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(First5Fragment.this)
+                        .navigate(R.id.action_First5Fragment_to_Second5Fragment);
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/gardenhand/GardenView.java
+++ app/src/main/java/com/example/gardenhand/GardenView.java
@@ -1,5 +1,6 @@
 package com.example.gardenhand;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -15,14 +16,20 @@ public class GardenView extends AppCompatActivity {
         setContentView(R.layout.activity_garden_view);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    public void galleryButtonClick (View view){
+        Intent intent = new Intent(this, GardenGallery.class);
+        startActivity(intent);
+    }
+
+    public void historyButtonClick (View view){
+        Intent intent = new Intent(this, GardenHistory.class);
+        startActivity(intent);
+    }
+
+    public void wishlistButtonClick (View view){
+        Intent intent = new Intent(this, PlantWishList.class);
+        startActivity(intent);
     }
 }
