4e8d09d18a800613fcbbbb6c6d23ae1f94837378
Casey Jonas <cjonas1999@gmail.com>
2020-12-07 19:37:57 -0600

    Implement Find Vendor Activity

--- app/src/main/java/com/example/gardenhand/SearchView.java
+++ app/src/main/java/com/example/gardenhand/SearchView.java
@@ -1,34 +1,44 @@
 package com.example.gardenhand;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
-import com.google.android.material.tabs.TabLayout;
-import androidx.viewpager.widget.ViewPager;
-import android.view.View;
 
-import com.example.gardenhand.ui.main.PlantTabsPagerAdapter;
-import com.example.gardenhand.ui.main.SectionsPagerAdapter;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SearchView extends AppCompatActivity {
+
+    Button searchButton;
+    EditText textBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_view);
-        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-        ViewPager viewPager = findViewById(R.id.view_pager);
-        viewPager.setAdapter(sectionsPagerAdapter);
-        TabLayout tabs = findViewById(R.id.tabs);
-        tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = findViewById(R.id.fab);
 
-        fab.setOnClickListener(new View.OnClickListener() {
+        textBox = (EditText) findViewById(R.id.vendor_text);
+        searchButton = (Button) findViewById(R.id.vendor_button);
+        
+        searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                String item = textBox.getText().toString();
+                if (item.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "No item listed", Toast.LENGTH_SHORT).show();
+                } else {
+                    String searchURL = "https://www.amazon.com/s?url=search-alias%3Daps&field-keywords=";
+
+                    String[] s = item.split("\\s+");
+                    searchURL.concat(TextUtils.join("+", s));
+
+                    Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(searchURL));
+                    startActivity(browserIntent);
+                }
             }
         });
     }
