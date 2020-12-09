eaa8bf7986de5b4b9678ec367b575a1cef18d266
Tjpraka <Tjpraka@ilstu.edu>
2020-12-08 18:58:51 -0600

    made first activity launched GardenerLogin

--- app/src/main/java/com/example/gardenhand/NotificationBroadcast.java
+++ app/src/main/java/com/example/gardenhand/NotificationBroadcast.java
@@ -7,6 +7,8 @@ import android.content.Intent;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
+import com.example.gardenhand.ui.login.GardenerLogin;
+
 public class NotificationBroadcast extends BroadcastReceiver {
     public static final String ID = "Notification";
     public static final String TITLE = "Reminder";
@@ -15,7 +17,7 @@ public class NotificationBroadcast extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Intent notificationIntent = new Intent(context, MainActivity.class);
+        Intent notificationIntent = new Intent(context, GardenerLogin.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, notificationIntent, 0);
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, intent.getStringExtra(CHANNEL))
                 .setSmallIcon(R.drawable.ic_stat_flower)
