b59a93dab13603665306e2680bc1d7218bc03495
Casey Jonas <cjonas1999@gmail.com>
2020-12-10 11:45:20 -0600

    Fixing issues

--- app/src/main/java/com/example/gardenhand/GardenGallery.java
+++ app/src/main/java/com/example/gardenhand/GardenGallery.java
@@ -4,13 +4,16 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class GardenGallery extends AppCompatActivity {
 
