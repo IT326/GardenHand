513b90429751a6c926e1d45ae7d936f25bff4b6b
Tjpraka <Tjpraka@ilstu.edu>
2020-12-09 22:00:53 -0600

    remove plants

--- app/src/main/java/com/example/gardenhand/PlantView.java
+++ app/src/main/java/com/example/gardenhand/PlantView.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -16,6 +19,7 @@ import android.widget.Toast;
 import com.example.gardenhand.ui.login.GardenerLogin;
 import com.squareup.picasso.Picasso;
 
+import java.util.Arrays;
 import java.util.Date;
 
 public class PlantView extends AppCompatActivity {
@@ -30,6 +34,7 @@ public class PlantView extends AppCompatActivity {
         TextView createdate;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plant_view);
+        createNotificationChannels();
       plant = (Plant) this.getIntent().getSerializableExtra("plant");
         garden = (Garden) this.getIntent().getSerializableExtra("garden");
         gardener =(Gardener) this.getIntent().getSerializableExtra("gardener");
@@ -71,10 +76,11 @@ public class PlantView extends AppCompatActivity {
 
     public void createNotification(View view){
         plant.lastWater = new Date();
+
         Intent intent=new Intent(this,NotificationBroadcast.class);
-        intent.putExtra("plant",plant);
-        intent.putExtra("garden",garden);
-        intent.putExtra("gardener",gardener);
+       // intent.putExtra("plant",plant);
+        //intent.putExtra("garden",garden);
+       // intent.putExtra("gardener",gardener);
         intent.putExtra(NotificationBroadcast.TITLE,plant.commonname);
         intent.putExtra(NotificationBroadcast.MESSAGE,"Time to water "+ plant.commonname);
         intent.putExtra(NotificationBroadcast.CHANNEL,"plant");
@@ -83,6 +89,7 @@ public class PlantView extends AppCompatActivity {
         AlarmManager alarmManager= (AlarmManager) getSystemService(ALARM_SERVICE);
         long timeNow=System.currentTimeMillis();
         long offset=1000*plant.daystowater+1;
+        System.out.println(plant.daystowater + " " + offset);
         alarmManager.set(AlarmManager.RTC_WAKEUP,timeNow+offset,pendingIntent);
     }
     public void compareButtonClick(View view) {}
@@ -97,6 +104,24 @@ public class PlantView extends AppCompatActivity {
         intent.putExtra("garden",garden);
         intent.putExtra("gardener",gardener);
         startActivity(intent);
+    }
+    private void createNotificationChannels() {
+        // Create the NotificationChannels, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel plantChannel = new NotificationChannel("plant", getString(R.string.plant_channel), NotificationManager.IMPORTANCE_HIGH);
+            plantChannel.setDescription(getString(R.string.plant_channel_description));
+            plantChannel.setShowBadge(false);
+            NotificationChannel socialChannel = new NotificationChannel("social", getString(R.string.social_channel), NotificationManager.IMPORTANCE_DEFAULT);
+            socialChannel.setDescription(getString(R.string.plant_channel_description));
+
+            NotificationChannel miscChannel = new NotificationChannel("misc", getString(R.string.misc_channel), NotificationManager.IMPORTANCE_DEFAULT);
+            miscChannel.setDescription(getString(R.string.plant_channel_description));
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannels(Arrays.asList(plantChannel,socialChannel,miscChannel));
+        }
     }
 
 }
